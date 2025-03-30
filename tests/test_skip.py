4cc81d3a99b8accb58135d52361e70b72496d4f0
artur.balsis <artur.balsis@gmail.com>
2025-03-30 15:11:03 +0300

    implement test_skip.py

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -3,16 +3,34 @@
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 import pytest
+from selene import browser
 
 
-@pytest.fixture()
-def browser():
-    pass
+def is_mobile(resolution):
+    width, height = resolution
+    return width <= 1024
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.fixture(scope = "function",
+                params = [(1920, 1080), (360, 800)],
+                ids = lambda res: f'{res[0]}x{res[1]}')
+def setup_browser(request):
+    width, height = request.param
+    browser.config.window_width, browser.config.window_height = width, height
+    yield "desktop" if width >= 900 else "mobile"
+    browser.quit()
 
 
-def test_github_mobile(browser):
-    pass
+def test_github_desktop(setup_browser):
+    if setup_browser == "mobile":
+        pytest.skip(reason = "It is mobile resolution")
+    browser.open('https://github.com')
+    browser.element('.HeaderMenu-link--sign-up').click()
+
+
+def test_github_mobile(setup_browser):
+    if setup_browser == "desktop":
+        pytest.skip(reason = "It is desktop resolution")
+    browser.open('https://github.com')
+    browser.element('.Button-label').click()
+    browser.element('.HeaderMenu-link--sign-up').click()
